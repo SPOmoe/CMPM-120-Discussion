fe261645ad7749a6ec25614e502ee3bfd63f0b6d
Marlene Inoue <mminoue@ucsc.edu>
2023-04-18 16:54:28 -0700

    added game over

--- Week 3/src/scenes/Play.js
+++ Week 3/src/scenes/Play.js
@@ -43,9 +43,44 @@ class Play extends Phaser.Scene {
       frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
       frameRate: 30
     });
+
+    // initialize score
+    this.p1Score = 0;
+
+    // display score
+    let scoreConfig = {
+      fontFamily: 'Courier',
+      fontSize: '28px',
+      backgroundColor: '#F3B141',
+      color: '#843695',
+      align: 'right',
+      padding: {
+        top: 5,
+        bottom: 5,
+      },
+      fixedWidth: 100
+    }
+
+    this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
+
+    // GAME OVER flag
+    this.gameOver = false;
+
+    // 60 second play clock
+    scoreConfig.fixedWidth = 0;
+    this.clock = this.time.delayedCall(69000, () => {
+      this.add.text(game.config.width / 2, game.config.height / 2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+      this.add.text(game.config.width / 2, game.config.height / 2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+      this.gameOver = true;
+    }, null, this);
   }
 
   update () {
+    // check key input for restart
+    if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+      this.scene.restart();
+    }
+
     this.p1Rocket.update();
     this.starfield.tilePositionX -= 4;
     this.ship01.update();
@@ -75,6 +110,13 @@ class Play extends Phaser.Scene {
     } else {
       return false;
     }
+
+    if (!this.gameOver) {
+      this.p1Rocket.update();
+      this.ship01.update();
+      this.ship02.update();
+      this.ship03.update();
+    }
   }
 
   shipExplode(ship) {
@@ -93,5 +135,9 @@ class Play extends Phaser.Scene {
       ship.alpha = 1; // make ship visible again
       boom.destroy(); // remove explosion sprite
     });
+
+    // score add and repaint
+    this.p1Score += ship.points;
+    this.scoreLeft.text = this.p1Score;
   }
 }
